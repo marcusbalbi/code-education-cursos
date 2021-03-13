61f361637198ca7355bca9101b9b820b69fa20f3
Marcus Balbi <marcus.balbi@a4pm.com.br>
2021-03-12 22:44:16 -0300

    get accessTOken

--- keycloak/goclient/main.go
+++ keycloak/goclient/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"log"
 	"net/http"
 
@@ -34,6 +35,32 @@ func main() {
 
 	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
 		http.Redirect(writer, request, config.AuthCodeURL(state), http.StatusFound)
+	})
+
+	http.HandleFunc("/auth/callback", func(writer http.ResponseWriter, request *http.Request) {
+		if request.URL.Query().Get("state") != state {
+			http.Error(writer, "State Invalido", http.StatusBadRequest)
+			return
+		}
+		token, err := config.Exchange(ctx, request.URL.Query().Get("code"))
+		if err != nil {
+			http.Error(writer, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		resp := struct {
+			AccessToken *oauth2.Token
+		}{
+			AccessToken: token,
+		}
+
+		data, err := json.Marshal(resp)
+		if err != nil {
+			http.Error(writer, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		writer.Write(data)
 	})
 
 	log.Fatal(http.ListenAndServe("localhost:8081", nil))
