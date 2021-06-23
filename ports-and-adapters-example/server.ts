e5c9ad5bee8cf0be0cb62304ef2832d52c572c15
Marcus Balbi <balbimarcus@gmail.com>
2021-06-23 20:36:40 -0300

    add new routes

--- ports-and-adapters-example/server.ts
+++ ports-and-adapters-example/server.ts
@@ -8,8 +8,9 @@ const app = express();
 
 connectionFactory('app.db').then(() => console.log('connected'));
 app.use(morgan(':method :url :status :response-time ms'));
+app.use(express.json());
 
-app.get('/get/:id', async (req: Request, res: Response) => {
+app.get('/product/:id', async (req: Request, res: Response) => {
   try {
     const persistense = new ProductPersistenceSqliteAdapter();
     const service = new ProductService(persistense);
@@ -19,14 +20,44 @@ app.get('/get/:id', async (req: Request, res: Response) => {
     res.status(500).json({ error: error.message });
   }
 });
-app.get('/save', async (req: Request, res: Response) => {
+
+app.post('/product', async (req: Request, res: Response) => {
   try {
     const persistense = new ProductPersistenceSqliteAdapter();
     const service = new ProductService(persistense);
-    const data = await service.create('Random PRoduct', 25.4);
+    const { name, price } = req.body;
+    const data = await service.create(name, price);
     res.json(data);
   } catch (error) {
-    res.status(500).json({ error: error.message, extras: error.getErrors() });
+    res
+      .status(500)
+      .json({ error: error.message, extras: error.getErrors ? error.getErrors() : null });
+  }
+});
+
+app.put('/product/:id/enable', async (req: Request, res: Response) => {
+  try {
+    const persistense = new ProductPersistenceSqliteAdapter();
+    const service = new ProductService(persistense);
+    const product = await service.get(req.params.id);
+    product.setPrice(req.body.price);
+    const result = await service.enable(product);
+    res.json(result);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.put('/product/:id/disable', async (req: Request, res: Response) => {
+  try {
+    const persistense = new ProductPersistenceSqliteAdapter();
+    const service = new ProductService(persistense);
+    const product = await service.get(req.params.id);
+    product.setPrice(0);
+    const result = await service.disable(product);
+    res.json(result);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 });
 
