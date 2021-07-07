20f392ed7cd6f65efd8e5d0e4167026bbfcddefe
Marcus Balbi <balbimarcus@gmail.com>
2021-07-06 21:46:21 -0300

    add times

--- kafka-node-example/src/adapters/commands/producer.ts
+++ kafka-node-example/src/adapters/commands/producer.ts
@@ -15,27 +15,30 @@ producer.connect();
 
 producer.on('ready', () => {
   console.log('Producer Ready!');
+  const times = argv.times || 1;
   try {
-    const payload = Buffer.from(JSON.stringify({ message: argv.message }));
-    const topic = 'teste';
-    const partition = null;
-    const key = 'KAFKA_EXAMPLE';
-    producer.produce(
-      // Topic to send the message to
-      topic,
-      // optionally we can manually specify a partition for the message
-      // this defaults to -1 - which will use librdkafka's default partitioner (consistent random for keyed messages, random for unkeyed messages)
-      partition,
-      // Message to send. Must be a buffer
-      payload,
-      // for keyed messages, we also specify the key - note that this field is optional
-      key,
-      // you can send a timestamp here. If your broker version supports it,
-      // it will get added. Otherwise, we default to 0
-      Date.now(),
-      // you can send an opaque token here, which gets passed along
-      // to your delivery reports
-    );
+    for (let i = 0; i < times; i++) {
+      const payload = Buffer.from(JSON.stringify({ message: argv.message, time: i }));
+      const topic = 'teste';
+      const partition = null;
+      const key = null;
+      producer.produce(
+        // Topic to send the message to
+        topic,
+        // optionally we can manually specify a partition for the message
+        // this defaults to -1 - which will use librdkafka's default partitioner (consistent random for keyed messages, random for unkeyed messages)
+        partition,
+        // Message to send. Must be a buffer
+        payload,
+        // for keyed messages, we also specify the key - note that this field is optional
+        key,
+        // you can send a timestamp here. If your broker version supports it,
+        // it will get added. Otherwise, we default to 0
+        Date.now(),
+        // you can send an opaque token here, which gets passed along
+        // to your delivery reports
+      );
+    }
   } catch (err) {
     console.log('Error:' + err.message);
   }
@@ -49,7 +52,9 @@ producer.on('event.error', function (err) {
 producer.on('delivery-report', function (err, report) {
   // Report of delivery statistics here:
   console.log(report);
-  console.log('key:' + report.key.toString());
+  if (report.key) {
+    console.log('key:' + report.key.toString());
+  }
 });
 
 producer.setPollInterval(100);
