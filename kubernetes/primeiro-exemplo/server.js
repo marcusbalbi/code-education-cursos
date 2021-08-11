0755f3d8d5abe20b82b5cef30b4d639e1c46edcc
Marcus Balbi <balbimarcus@gmail.com>
2021-08-10 21:53:13 -0300

    working wiht env on deployment

--- kubernetes/primeiro-exemplo/server.js
+++ kubernetes/primeiro-exemplo/server.js
@@ -5,6 +5,12 @@ const app = express();
 app.get("/", (req, res) => {
   res.json({ message: "Hello from Full Cycle" });
 });
+app.get("/info", (req, res) => {
+  res.json({
+    app_name: process.env.APP_NAME,
+    app_version: process.env.APP_VERSION,
+  });
+});
 
 app.listen(3000, () => {
   console.log("Servidor iniciado na porta 3000");
