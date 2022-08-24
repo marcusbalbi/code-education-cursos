54f6d7ae9c557c6018fdea29f74736eccbf6701c
Marcus Balbi <balbimarcus@gmail.com>
2022-08-23 21:08:05 -0300

    Order dd tatico

---
+++ ddd-tatico/src/main.ts
@@ -0,0 +1,14 @@
+import { Address } from "./entity/Address";
+import { Customer } from "./entity/Customer";
+import Order from "./entity/Order";
+import OrderItem from "./entity/OrderItem";
+
+let customer = new Customer("123", "Balbi");
+const address = new Address("Alameda bla bla", 12, "28625000", "Nova Friburgo");
+customer.defineAddress(address);
+
+
+const item1 = new OrderItem("1", "Caneta Azul", 0.5);
+const item2 = new OrderItem("2", "Caneta Rosa", 0.7);
+const order = new Order('1', customer._id, [item1, item2]);
+
